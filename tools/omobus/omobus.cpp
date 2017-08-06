59b24792f1f477cf760006bdb6275040704ee6a9
Igor Artemov <i_artemov@ak-obs.ru>
2017-08-06 10:25:25 +0300

    fixed copyrigth.

--- tools/omobus/omobus.cpp
+++ tools/omobus/omobus.cpp
@@ -3,8 +3,8 @@
  * Copyright (c) 2006 - 2013 ak-obs, Ltd. <info@omobus.net>.
  * Author: Igor Artemov <i_artemov@ak-obs.ru>.
  *
- * This program is commerce software; you can't redistribute it and/or
- * modify it.
+ * This program is a free software. Redistribution and use in source and binary 
+ * forms, with or without modification, are permitted provided under the GPLv2 license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- tools/today-plugin/TodayWindow.cpp
+++ tools/today-plugin/TodayWindow.cpp
@@ -2,10 +2,9 @@
 /* This file is a part of the omobus-mobile project.
  * Copyright (c) 2006 - 2013 ak-obs, Ltd. <info@omobus.net>.
  * Author: Igor Artemov <i_artemov@ak-obs.ru>.
-
  *
- * This program is commerce software; you can't redistribute it and/or
- * modify it.
+ * This program is a free software. Redistribution and use in source and binary
+ * forms, with or without modification, are permitted provided under the GPLv2 license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
